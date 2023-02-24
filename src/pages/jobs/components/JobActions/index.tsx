0cb22f46ca8a7aa0a178781f405d7bc281cc1ca4
chrisnph <chris.ng860624@gmail.com>
2023-02-24 14:05:01 +0800

    style(modal): update UI

--- src/pages/jobs/components/JobActions/index.tsx
+++ src/pages/jobs/components/JobActions/index.tsx
@@ -2,7 +2,7 @@ import JobApply from "./JobApply";
 import JobShare from "./JobShare";
 
 const JobActions: React.FC = () => (
-  <div className="jobDetails-actions flex justify-center md:justify-end items-end w-[420px] sm:mt-5">
+  <div className="jobDetails-actions flex justify-center md:justify-end items-end w-full md:w-[420px] sm:mt-5 pl-12">
     <div className="block md:flex w-fit">
       <div className="mt-5 justify-center md:mt-0 md:w-1/2">
         <JobApply />
